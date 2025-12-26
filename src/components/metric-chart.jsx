d93671100eae852ddc029d7f8e663336b6b55e6d
Baginski <kulanaryoseu@proton.me>
2025-12-26 10:39:07 -0800

    metric chart refactor

--- src/components/metric-chart.jsx
+++ src/components/metric-chart.jsx
@@ -6,41 +6,18 @@ import SingleLineChart from "./charts/single-line";
 import StackedBarChart from "./charts/stacked-bar";
 import NoData from "./no-data";
 
-export default function MetricChart({ data, chartConfig, chartSeries }) {
-  const { metrics, type, settings } = chartConfig;
-  let series = [];
+export default function MetricChart({ series, chartConfig }) {
 
   const ChartComponent =
-    type === "single"
+    chartConfig.type === "single"
       ? SingleLineChart
-      : type === "biaxial"
+      : chartConfig.type === "biaxial"
         ? BiaxialLineChart
-        : type === "stacked_bar"
+        : chartConfig.type === "stacked_bar"
           ? StackedBarChart
-          : type === "pie"
+          : chartConfig.type === "pie"
             ? CustomPieChart
             : NoData;
 
-  if (chartSeries) {
-    return <ChartComponent data={chartSeries} chartSettings={settings} />;
-  }
-
-  const records = data?.records ? [...data.records].reverse() : [];
-
-  series = records
-    ? records.map((record) => {
-        const result = { x: record.created_at };
-
-        metrics.forEach((metric) => {
-          const extracted = metric?.extractor(record);
-          result[metric?.key] = metric?.valuesTransform
-            ? metric.valuesTransform(extracted)
-            : extracted;
-        });
-
-        return result;
-      })
-    : [];
-
-  return <ChartComponent data={series} chartSettings={settings} />;
+  return <ChartComponent data={series} chartSettings={chartConfig.settings} />;
 }
